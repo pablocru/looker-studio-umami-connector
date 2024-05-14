48b5578a421601d29bfc2c9a87457cd6818525c8
Piterxus <piterxus@gmail.com>
2024-05-14 17:11:51 +0200

    chore: Configure Linter for Typescript files

--- ts-umami-connector/src/auth.ts
+++ ts-umami-connector/src/auth.ts
@@ -1,8 +1,8 @@
 // https://developers.google.com/datastudio/connector/reference#getauthtype
 const getAuthType: GetAuthType = () => {
-  const AuthTypes = cc.AuthType;
+  const AuthTypes = cc.AuthType
   return cc
     .newAuthTypeResponse()
     .setAuthType(AuthTypes.NONE)
-    .build();
-};
+    .build()
+}
--- ts-umami-connector/src/main.ts
+++ ts-umami-connector/src/main.ts
@@ -1,21 +1,21 @@
-const cc = DataStudioApp.createCommunityConnector();
+const cc = DataStudioApp.createCommunityConnector()
 
 // https://developers.google.com/datastudio/connector/reference#isadminuser
 const isAdminUser: IsAdminUser = () => {
-  return false;
-};
+  return false
+}
 
 // https://developers.google.com/datastudio/connector/reference#getconfig
 const getConfig: GetConfig = () => {
-  const config = cc.getConfig();
+  const config = cc.getConfig()
 
   config
     .newInfo()
     .setId('generalInfo')
     .setText(
-      'This is the template connector created by '+
+      'This is the template connector created by ' +
       'https://github.com/googledatastudio/dscc-gen'
-    );
+    )
 
   config
     .newSelectSingle()
@@ -34,71 +34,71 @@ const getConfig: GetConfig = () => {
         .newOptionBuilder()
         .setLabel('Imperial')
         .setValue('imperial')
-    );
+    )
 
-  return config.build();
-};
+  return config.build()
+}
 
 const getFields = (): Fields => {
-  const fields = cc.getFields();
-  const types = cc.FieldType;
-  const aggregations = cc.AggregationType;
+  const fields = cc.getFields()
+  const types = cc.FieldType
+  const aggregations = cc.AggregationType
 
   fields
     .newDimension()
     .setId('id')
     .setName('Id')
-    .setType(types.TEXT);
+    .setType(types.TEXT)
 
   fields
     .newMetric()
     .setId('distance')
     .setName('Distance')
     .setType(types.NUMBER)
-    .setAggregation(aggregations.SUM);
+    .setAggregation(aggregations.SUM)
 
-  return fields;
-};
+  return fields
+}
 
 // https://developers.google.com/datastudio/connector/reference#getschema
 const getSchema: GetSchema = () => {
-  return {schema: getFields().build()};
-};
+  return { schema: getFields().build() }
+}
 
 // https://developers.google.com/datastudio/connector/reference#getdata
 const getData: GetData = (request) => {
   // Calling `UrlFetchApp.fetch()` makes this connector require authentication.
-  UrlFetchApp.fetch('https://google.com');
+  UrlFetchApp.fetch('https://google.com')
 
   const requestedFields = getFields().forIds(
     request.fields.map((field) => field.name)
-  );
+  )
 
   // Convert from miles to kilometers if 'metric' units were picked.
-  let unitMultiplier = 1;
+  let unitMultiplier = 1
   if (request.configParams.units === 'metric') {
-    unitMultiplier = 1.60934;
+    unitMultiplier = 1.60934
   }
 
-  const rows: GetDataRows = [];
+  const rows: GetDataRows = []
   for (let i = 0; i < 100; i++) {
-    const rowValues: GetDataRowValue[] = [];
+    const rowValues: GetDataRowValue[] = []
     requestedFields.asArray().forEach((field) => {
       switch (field.getId()) {
         case 'id':
-          return rowValues.push('id_' + i);
+          return rowValues.push('id_' + i)
         case 'distance':
-          return rowValues.push(i * unitMultiplier);
+          return rowValues.push(i * unitMultiplier)
         default:
-          return rowValues.push('');
+          return rowValues.push('')
       }
-    });
-    const row: GetDataRow = {values: rowValues};
-    rows.push(row);
+    })
+    const row: GetDataRow = { values: rowValues }
+    rows.push(row)
   }
 
   return {
     schema: requestedFields.build(),
-    rows: rows,
-  };
-};
+    rows
+  }
+}
