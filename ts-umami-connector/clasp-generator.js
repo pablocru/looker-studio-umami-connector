fba6dcee6c295abb7021c872064bb2c0e8f8b9fb
pablocru <pablocru@outlook.com>
2024-05-14 11:39:25 +0200

    feat: Initiate standalone Apps Script Community Connector with TypeScript

---
+++ ts-umami-connector/clasp-generator.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const jsonString = JSON.stringify({
+  scriptId: process.env.APPS_SCRIPT_ID,
+  rootDir:"src"
+});
+
+fs.writeFileSync('.clasp.json', jsonString, 'utf-8');
