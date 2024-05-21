1e42597daace08531faca8fb8af587fdb6521b47
pablocru <pablocru@outlook.com>
2024-05-21 15:09:08 +0200

    feat: Create a schema for each api paths of website stats endpoint

--- ts-umami-connector/src/schema.ts
+++ ts-umami-connector/src/schema.ts
@@ -29,16 +29,100 @@ function getSchemaOfEvents(): GoogleAppsScript.Data_Studio.Fields {
   return fields;
 }
 
+function getSchemaOfPageViews(): GoogleAppsScript.Data_Studio.Fields {
+  const fields = cc.getFields();
+  const types = cc.FieldType;
+
+  fields.newDimension()
+    .setId("timestamp")
+    .setType(types.TEXT);
+
+  fields.newDimension()
+    .setId("number_of_visitors")
+    .setType(types.NUMBER);
+
+  return fields;
+}
+
+function getSchemaOfMetrics(): GoogleAppsScript.Data_Studio.Fields {
+  const fields = cc.getFields();
+  const types = cc.FieldType;
+
+  fields.newDimension()
+    .setId("metric_type")
+    .setType(types.TEXT);
+
+  fields.newDimension()
+    .setId("number_of_visitors")
+    .setType(types.NUMBER);
+
+  return fields;
+}
+
+function getSchemaOfStats(): GoogleAppsScript.Data_Studio.Fields {
+  const fields = cc.getFields();
+  const types = cc.FieldType;
+
+  fields.newDimension()
+    .setId("page_views_value")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("page_views_change")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("visitors_value")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("visitors_change")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("visits_value")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("visits_change")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("bounces_value")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("bounces_change")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("total_time_value")
+    .setType(types.NUMBER);
+
+  fields.newDimension()
+    .setId("total_time_change")
+    .setType(types.NUMBER);
+  return fields;
+}
+
 function getSchema(request: CCRequest): SchemaResponse {
   const apiPath = request.configParams[API_PATH_ID];
 
   let fields: GoogleAppsScript.Data_Studio.Fields | undefined;
   switch (apiPath) {
+    case ACTIVE_PATH:
+      fields = getSchemaOfActiveUsers();
+      break;
     case EVENTS_PATH:
       fields = getSchemaOfEvents();
       break;
-    case ACTIVE_PATH:
-      fields = getSchemaOfActiveUsers();
+    case PAGE_VIEW_PATH:
+      fields = getSchemaOfPageViews();
+      break;
+    case STATS_PATH:
+      fields = getSchemaOfStats();
+    case METRICS_PATH:
+      fields = getSchemaOfMetrics();
       break;
     default:
       cc.newUserError().setText("Invalid API path: " + apiPath).throwException();
